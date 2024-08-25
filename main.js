7ba9c8913000825add75a612bf665a7cde875aa8
Abdulaziz Ergashev <eabdulaziz36@gmail.com>
2024-08-25 09:39:37 +0500

    Culmination Tasks in the first month of JS

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-// 5. Arrey ichidagi juft raqamlarni boshqa bir arrey ga solib qaytaruvchi function yarating 
 // 6. Arrey ichidagi objectlarni id ga ko’ra malumotlarini chiqaruvchi function 
 // 7. String holda raqamlar beriladi va uni teskari arrayga aylantiruvchi funksiya tuzing. Misol uchun: "34567" => [7, 6, 5, 4, 3]
 
@@ -81,16 +80,74 @@
 
 
 // TASK 4 ||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
-// 4. arrey ichida objectlar bor userda input orqali qiymat olib arrey ichidagi object dan shu qiymatga teng bo’lgan obj topish 
+// 4. arrey ichida objectlar bor userdan input orqali qiymat olib arrey ichidagi object dan shu qiymatga teng bo’lgan obj topish 
 
-let arr = [
-    {
-        name: 'Abdulaziz',
-        id: 23733
-    },
-    {
-        name: 'Muhammadjon',
-        id: 23734
-    }
-];
+
+// Using array find method 
+
+// let inputValue = prompt('Enter any info of the user');
+// let arr = [
+//     {
+//         name: 'Abdulaziz',
+//         id: 23733
+//     },
+//     {
+//         name: 'Muhammadjon',
+//         id: 23734
+//     }
+// ];
+
+// const findTheCorrespondingValue = (arr) => {
+//     for (let i = 0; i < arr.length; i++) {
+//         let values = Object.values(arr[i]);
+//         let findTheValue = values.find((item) => item == inputValue) // item objectni ichidagi asl data type, 
+//         // inputValue bu promptda olingani sabab string. Lekin biz strictmas tenglik qo'yganmiz (==). Shunga to'gri chiqyapti
+//         if (findTheValue) {
+//             return arr[i];
+//         }
+//     }
+//     return 'Error';
+// }
+// console.log(findTheCorrespondingValue(arr));
+
+
+
+
+// Using inner loops
+
+// let inputValue = prompt('Enter any info of the user');
+// let arr = [
+//     {
+//         name: 'Abdulaziz',
+//         id: 23733
+//     },
+//     {
+//         name: 'Muhammadjon',
+//         id: 23734
+//     }
+// ];
+// const findTheCorrespondingValue = (arr) => {
+//     let response = 'Error';
+//     for (let i = 0; i < arr.length; i++) { // i = 0, 1
+//         let values = Object.values(arr[i]);
+//         for (let k = 0; k < values.length; k++) {
+//             if (values[k] == inputValue) {
+//                 response = arr[i];
+//             }
+//         }
+//     }
+//     return response;
+// }
+// console.log(findTheCorrespondingValue(arr));
+
+
+
+
+
+
+
+
+
+// TASK 5 ||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+// 5. Arrey ichidagi juft raqamlarni boshqa bir arrey ga solib qaytaruvchi function yarating 
 
